d4a2395a39d23973e69f613999ed74d5015a47de
Maruf Hasan Anik <111722365+Skippersquareroot100@users.noreply.github.com>
2025-07-21 10:15:53 +0600

    user role updtae to customer

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
 import { EmployeeModule } from './employee/employee.module';
 import { ManagerModule } from './manager/manager.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [UserModule, AdminModule, EmployeeModule, ManagerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AdminModule, EmployeeModule, ManagerModule, CustomerModule],
 })
 export class AppModule {}
