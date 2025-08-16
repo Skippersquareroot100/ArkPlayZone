4d319bd5e390f1175b10b4b79c5f8f793a77f7aa
swajan-75 <swajanbarua09@gmail.com>
2025-08-16 13:21:30 +0600

    CURD and forgot password has been added to customer

--- nest-backend/src/mailer/mailer.module.ts
+++ nest-backend/src/mailer/mailer.module.ts
@@ -4,20 +4,18 @@ import { MailerController as MailController } from './mailer.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        ignoreTLS: true,
-        auth: {
-          user: 'hasanmaruf0055@gmail.com',
-          pass: '',
+    imports: [MailerModule.forRoot({
+        transport: {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            ignoreTLS : true, 
+            auth: {
+                user: 'swajanbarua09@gmail.com',
+                pass: '', 
+            },
         },
-      },
-    }),
-  ],
+    })],
   providers: [MailService],
   controllers: [MailController],
   exports: [MailService],
